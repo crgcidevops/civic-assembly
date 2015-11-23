cb2cd02eb6a485167e39b89717ce222b3231c172
mihasK <mihas-91@tut.by>
2015-11-23 18:43:57 +0300

    local settings update

--- local_settings.py
+++ local_settings.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# Setup filesystem storage fo static and media
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 THUMBNAIL_DEFAULT_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATIC_ROOT = '/home/.static/'
+MEDIA_ROOT = '/home/.media/'
 
+#######   filesystem storage end      #############
 
 EVENTS_ALLOWED_RELATED_MODELS = ['venues', 'organizations']
 
-STATIC_ROOT = '/home/.static/'
-MEDIA_ROOT = '/home/.media/'
 DEBUG = False
 GLOBAL_FOLDERS = ''
