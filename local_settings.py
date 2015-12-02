750d20208ae6fa4c913d63366db53577d9ec5d5c
mihasK <mihas-91@tut.by>
2015-12-02 14:39:06 +0300

    fake-s3

--- local_settings.py
+++ local_settings.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Setup filesystem storage fo static and media
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-THUMBNAIL_DEFAULT_STORAGE = 'django.core.files.storage.FileSystemStorage'
-STATIC_ROOT = '/home/.static/'
-MEDIA_ROOT = '/home/.media/'
-
-#######   filesystem storage end      #############
+DEBUG = False
 
 EVENTS_ALLOWED_RELATED_MODELS = ['venues', 'organizations']
 
-DEBUG = False
+AWS_STORAGE_BUCKET_NAME = 'civic-local-bucket'
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com'
+AWS_S3_SECURE_URLS = False
+AWS_S3_PORT = 4569
