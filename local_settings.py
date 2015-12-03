d20f8733a325d0de3a8a86ddb5c96acdd330658b
Dmitry Vechorko <vechorkodima@gmail.com>
2015-12-03 14:03:28 +0300

    Disable mtc applications

--- local_settings.py
+++ local_settings.py
@@ -12,3 +12,24 @@ MEDIA_ROOT = '/home/.media/'
 EVENTS_ALLOWED_RELATED_MODELS = ['venues', 'organizations']
 
 DEBUG = False
+
+if 'test' not in sys.argv:
+	mtc_applications = [
+	    'venues_bike_share_station',
+	    'venues_traffic_camera',
+	    'venues_hov_lane',
+	    'venues_toll_point',
+	    'venues_traffic_sign',
+	    'venues_transit_stop'
+	]
+
+
+	from django.conf import settings
+
+	print len(settings.INSTALLED_APPS)
+
+	for mtc_app in [app for app in mtc_applications if app in settings.INSTALLED_APPS]:
+		settings.INSTALLED_APPS.remove(mtc_app)
+
+	print "\n", "*"*50, "\nMTC applications was deleted from INSTALL_APPS\n", "*"*50, "\n"
+	print len(settings.INSTALLED_APPS)
